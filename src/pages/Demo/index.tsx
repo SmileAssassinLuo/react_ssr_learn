9a6fe6ff9514c0a313dab61e5e6dccee4878ab05
luokai <lk_assassin@formssi.com>
2023-03-21 14:44:19 +0800

    数据脱水注水

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -1,7 +1,71 @@
-import { FC } from "react";
+import { FC ,useState,useEffect ,Fragment} from "react";
+import axios  from 'axios';
+import {connect } from 'react-redux'
+import {getDemoData} from "./Store/demoReducer"
+import { Helmet } from 'react-helmet'
 
-const Demo: FC = () => {
-  return <div>这是一个demo页面</div>;
-};
+// 可以在 network 中看到对应的请求，数据也没在服务器端请求的时候塞入 HTML，也就是说走的是客户端渲染，
+//而不是服务端渲染，和我们预期的不一样，看来是不能直接用 hook 来常规请求的。
+// const Demo: FC = () => {
+//   const [content, setContent] = useState("")
+//   useEffect(() => {
+//     axios.post("api/getDemoData",{
+//       content:"这是一个demo页面!!!"
+//     })
+//     .then((res:any) => {
+//       setContent(res.data?.data?.content)
+//     })
+//   })
+//   return <div>{content}</div>
+// };
 
-export default Demo;
+
+//connect 暴露了两个参数，一个 state，一个 dispatch，它会根据你的需要拼接成指定的参数，以装饰器的形式包装你定义的函数，这样我们的 Demo 就可以接收到我们定义的 content 和 getDemoData 参数了
+interface IProps {
+  content?:string;
+  getDemoData?:(data:string) => void
+}
+const Demo:FC<IProps> = (data) => {
+
+    return (
+        <Fragment>
+            <Helmet>
+              <title>服务器端渲染框架-demo</title>
+              <meta name="description" content="服务器端渲染框架"></meta>
+            </Helmet>
+            <div>
+              <h1>{data.content}</h1>
+              <button 
+                  onClick = { () : void => {
+                    data.getDemoData && data.getDemoData("刷新过后的数据")
+                  }}
+              >
+                  刷新
+              </button>
+            </div>
+        </Fragment>
+    )
+}
+
+// 将对应reducer的内容透传回dom
+const mapStateToProps = (state:any) => {
+  return {
+      content:state?.demo?.content
+  }
+}
+
+const mapDispatchToProps = (dispatch:any) => {
+  return {
+    getDemoData:(data:string) => {
+      dispatch(getDemoData(data))
+    }
+  }
+}
+const storeDemo:any = connect(mapStateToProps,mapDispatchToProps)(Demo)
+
+storeDemo.getInitProps = (store:any,data?:string) => {
+  return store.dispatch(getDemoData(data || "这是初始化的demo"))
+}
+
+
+export default storeDemo;
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -3,7 +3,7 @@ import childProcess from "child_process";
 import { renderToString } from "react-dom/server";
 import path from "path";
 // 引入路由相关
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,matchRoutes, RouteObject } from "react-router-dom";
 import { StaticRouter } from "react-router-dom/server";
 import router from "@/router";
 import { Helmet } from "react-helmet"; //修改页面header
@@ -30,33 +30,68 @@ app.post("/api/getDemoData", (req, res) => {
   });
 });
 
-app.get("*", (req, res) => {
-  const content = renderToString(
-    <Provider store={serverStore}>
-      <StaticRouter location={req.path}>
-        <Routes>
-          {router?.map((item, index) => {
-            return <Route {...item} key={index} />;
-          })}
-        </Routes>
-      </StaticRouter>
-    </Provider>
-  );
+// app.get("*", (req, res) => {
+//   const content = renderToString(
+//     <Provider store={serverStore} children={undefined}>
+//       <StaticRouter location={req.path}>
+//         <Routes>
+//           {router?.map((item, index) => {
+//             return <Route {...item} key={index} />;
+//           })}
+//         </Routes>
+//       </StaticRouter>
+//     </Provider>
+//   );
+  
+  //在服务器端拉取对应的初始化方法，并统一请求注入它们了，我们只需要改造 get 方法就可以，遍历所有的初始化方法，然后统一请求塞进 store 里
+  app.get("*", (req, res) => {
+    const routeMap = new Map<string, () => Promise<any>>(); // path - loaddata 的map
+    router.forEach((item) => {
+      if (item.path && item.loadData) {
+        routeMap.set(item.path, item.loadData(serverStore));
+      }
+    });
+  
+    // 匹配当前路由的routes
+    const matchedRoutes = matchRoutes(router as RouteObject[], req.path);
+  
+    const promises: Array<() => Promise<any>> = [];
+    matchedRoutes?.forEach((item) => {
+      if (routeMap.has(item.pathname)) {
+        promises.push(routeMap.get(item.pathname) as () => Promise<any>);
+      }
+    });
+  
+    Promise.all(promises).then((data) => {
+      // 统一放到state里
+      // 编译需要渲染的JSX, 转成对应的HTML STRING
+      const content = renderToString(
+        <Provider store={serverStore} children={""}>
+          <StaticRouter location={req.path}>
+            <Routes>
+              {router?.map((item, index) => {
+                return <Route {...item} key={index} />;
+              })}
+            </Routes>
+          </StaticRouter>
+        </Provider>
+      );
 
-  const helmet = Helmet.renderStatic();
+    const helmet = Helmet.renderStatic();
 
-  res.send(`
-    <html
-      <head>
-        ${helmet.title.toString()}
-        ${helmet.meta.toString()}
-      </head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="/index.js"></script>
-      </body>
-    </html>
-  `);
+    res.send(`
+      <html>
+        <head>
+          ${helmet.title.toString()}
+          ${helmet.meta.toString()}
+        </head>
+        <body>
+          <div id="root">${content}</div>
+          <script src="/index.js"></script>
+        </body>
+      </html>
+    `);
+  });
 });
 
 app.listen(3000, () => {
